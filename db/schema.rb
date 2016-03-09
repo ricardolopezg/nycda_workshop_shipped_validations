9d2c526242c79d86ddb1a45e4aa87c6f88901c8a
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-03-09 15:01:57 -0500

    added job title to jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309174934) do
+ActiveRecord::Schema.define(version: 20160309200047) do
 
   create_table "boats", force: :cascade do |t|
     t.integer  "profile_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160309174934) do
     t.datetime "delivery_date"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "title"
   end
 
   create_table "profiles", force: :cascade do |t|
