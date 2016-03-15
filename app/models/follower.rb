4c5502184130856345965e508cf9c796df388c0e
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-03-15 00:52:42 -0400

    profile editing update

--- app/models/follower.rb
+++
@@ -1,5 +0,0 @@
-class Follower < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :boat
-
-end
--- test/models/user_boat_test.rb
+++ test/models/user_boat_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class FollowerTest < ActiveSupport::TestCase
+class UserBoatTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
